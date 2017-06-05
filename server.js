3ddc8c1003b760df18f4e9b3e389d33a2ffbb5f0
Motke84 <motke.mat@gmail.com>
2017-06-05 14:46:59 +0300

    add heroku support

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(function (req, res, next){
+
+  if (req.protocol === 'http') {
+    console.log('1');
+    next();
+  } else {
+      console.log('2');
+    res.redirect('http://' + req.hostname + req.url);
+     
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(5000, function () {
-  console.log('Express server is up on port 5000');
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
